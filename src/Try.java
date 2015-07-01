d195f947bacba0ea2e6ea2c66858da347ab48c7a
unknown <Vivek.Bhardwaj@Vivek-PC.e-navik.local>
2015-07-01 11:10:33 +0200

    my first

---
+++ src/Try.java
@@ -0,0 +1,13 @@
+
+public class Try {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String as="ant";
+System.out.println("value=="+as);
+	}
+
+}
