b51d9e795cb417301658ce80e8434f9c9ca3d74c
unknown <Vivek.Bhardwaj@Vivek-PC.e-navik.local>
2015-07-01 08:49:16 +0200

    my first coomit

---
+++ src/A.java
@@ -0,0 +1,16 @@
+
+public class A {
+	public int ret()
+	{
+		int i=3;
+		return i;
+	}
+
+
+public String ret1()
+{
+	return "string return";
+	
+}
+
+}
---
+++ src/Other.java
@@ -0,0 +1,11 @@
+
+public class Other extends Simple  {
+public static void main(String j[]){
+	Simple a=new Simple();
+	a.number();
+	System.out.println(a.i);
+int p=a.fact(5);
+System.out.println("value of fact="+p);
+}
+	
+}
